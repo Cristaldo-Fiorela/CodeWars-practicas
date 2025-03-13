0ff0441037f85eb9fb8f5342b9b640a26f02d36d
Fiorela Cristaldo <cristaldofiorela@gmail.com>
2025-03-13 18:56:26 -0300

    nuevos ejercicios kata

---
+++ Javascript/03-kata.js
@@ -0,0 +1,28 @@
+/**
+ * 8 kyu
+  Are You Playing Banjo?
+
+  Create a function which answers the question "Are you playing banjo?".
+    If your name starts with the letter "R" or lower case "r", you are playing banjo!
+
+    The function takes a name as its only argument, and returns one of the following strings:
+
+    name + " plays banjo" 
+    name + " does not play banjo"
+    Names given are always valid strings.
+ */
+
+    function areYouPlayingBanjo(name) {
+      // Implement me
+      const toLower = name.toLowerCase();
+      const firstLetter = toLower.charAt(0)
+      
+      if(firstLetter == 'r') {
+        return name + " plays banjo" 
+      } else {
+        return name + " does not play banjo"
+      }
+    
+    }
+
+    areYouPlayingBanjo('fiorela');
